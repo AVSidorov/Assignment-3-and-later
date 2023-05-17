0299a902e00979ffa694b621e46abeaee1f74379
sid <sidorov@mail.ioffe.ru>
2023-05-17 16:53:25 +0200

    Assignment 9. test_ioctl.c added buffer cleanup between reads.

--- aesd-char-driver/test_ioctl.c
+++ aesd-char-driver/test_ioctl.c
@@ -24,12 +24,15 @@ int main(int argc, char* argv[]){
 
 	printf("set to command %d offset %d\n", seekto.write_cmd, seekto.write_cmd_offset);
 
-	char buf[BUF_SIZE] ;
+	char buf[BUF_SIZE];
+	memset(&buf, 0, BUF_SIZE);
+
 	int fd = open(FILENAME, O_CREAT | O_RDWR | O_APPEND | O_TRUNC | O_SYNC, 0644);
 
 	if (ioctl(fd, AESDCHAR_IOCSEEKTO, &seekto) == 0)
 		while (read(fd, &buf, BUF_SIZE)){
 			printf("%s", buf);
+			memset(&buf, 0, BUF_SIZE);
 		}
 	else
 		printf("ioctl error\n");
